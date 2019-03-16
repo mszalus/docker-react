e769c8113f83148952e5bd7fbe571fd0cf246712
mszalus <3ldrichT>
2019-03-17 00:20:44 +0100

    Commit

--- src/App.test.js
+++ src/App.test.js
@@ -10,19 +10,6 @@ it('renders without crashing', () => {
 
 
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-
 
 
 
